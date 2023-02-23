7bde0c5868cd75fd0957275d05b8d45287b121c5
tilaksatra <ayushsatra@gmail.com>
2023-02-23 18:53:17 -0500

    Updating testengine

--- test/testengine.py
+++ test/testengine.py
@@ -10,16 +10,19 @@ from data import *
 
 n = 0
 
+
 def test_the():
     # oo(options)
     print(options)
     return True
 
-def test_copy(): #copy
-    t1 = {'a' : 1, 'b' : { 'c' : 2, 'd' : [3]}}
+
+def test_copy():  # copy
+    t1 = {'a': 1, 'b': {'c': 2, 'd': [3]}}
     t2 = utils.copy(t1)
     t2['b']['d'][0] = 10000
     print('b4', t1, '\nafter', t2)
+
 
 def test_sym():
     symObj = Sym()
@@ -27,11 +30,13 @@ def test_sym():
         symObj.add(x)
     return "a" == symObj.mid() and 1.379 == rnd(symObj.div(), 3)
 
+
 def test_num():
     num = Num()
     for x in [1, 1, 1, 1, 2, 2, 3]:
         num.add(x)
     return 11 / 7 == num.mid() and 0.787 == rnd(num.div(), 3)
+
 
 def test_repCols():
     t = repCols(dofile(options['file'])['cols'])
@@ -39,6 +44,7 @@ def test_repCols():
         oo(col)
     for row in t.rows:
         oo(row)
+
 
 def test_repRows():
     t = dofile(options['file'])
@@ -48,14 +54,17 @@ def test_repRows():
     for row in rows.rows:
         oo(row)
 
+
 def test_synonyms():
     data = DATA(options['file'])
     show(repCols(dofile(options['file'])['cols']).cluster())
 
+
 def test_prototypes():
     t = dofile(options['file'])
     rows = repRows(t, transpose(t['cols']))
-    show(rows.cluster(),"mid",rows.cols.all,1)
+    show(rows.cluster(), "mid", rows.cols.all, 1)
+
 
 def test_position():
     t = dofile(options['file'])
@@ -63,8 +72,10 @@ def test_position():
     rows.cluster()
     repPlace(rows)
 
+
 def test_every():
     repgrid(options['file'])
+
 
 if __name__ == '__main__':
     eg('the', 'check the', test_the)
@@ -72,9 +83,9 @@ if __name__ == '__main__':
     eg('num', 'check nums', test_num)
     eg('sym', 'check syms', test_sym)
     eg('repcols', 'checking repcols', test_repCols)
-    eg('reprows','checking reprows', test_repRows)
-    eg('synonyms','checking repcols cluster', test_synonyms)
-    eg('prototypes','checking reprows cluster', test_prototypes)
-    eg('position','where\'s wally', test_position)
-    eg('every','the whole enchilada', test_every)
+    eg('reprows', 'checking reprows', test_repRows)
+    eg('synonyms', 'checking repcols cluster', test_synonyms)
+    eg('prototypes', 'checking reprows cluster', test_prototypes)
+    eg('position', 'where\'s wally', test_position)
+    eg('every', 'the whole enchilada', test_every)
     main(options, help, egs)
